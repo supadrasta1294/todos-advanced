0cc1326d047f23d5e47d65f7ca5ee6d967b3e87e
supadrasta1294 <supadrasta1294@gmail.com>
2019-06-27 22:32:58 -0400

    Added form in sign in and validations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { TodosComponent } from './todos/todos.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { TodosComponent } from "./todos/todos.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+
+
 
 const routes: Routes = [
   {
-    path:'',
-    redirectTo:'sign-in' ,
-    pathMatch:'full'   
+    path: "",
+    redirectTo: "sign-in",
+    pathMatch: "full"
   },
-{
-  path:'sign-in',component:SignInComponent},
-{path:'todos',component:TodosComponent},
-{path:'**',component:PageNotFoundComponent}
+  {
+    path: "sign-in",
+    component: SignInComponent
+  },
+  { path: "todos", component: TodosComponent },
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
